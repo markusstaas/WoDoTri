25cd33f9734a06936b54f3a32d20840bc295e8f7
Markus <markus.staas@me.com>
2018-02-21 12:34:07 +0000

    Improve readability of CoreDataStack class

--- WoDoTri/CoreDataStack.swift
+++ WoDoTri/CoreDataStack.swift
@@ -4,30 +4,29 @@ import CoreData
 
 final class CoreDataStack {
 
-        static let persistentContainer: NSPersistentContainer = {
-            let container = NSPersistentContainer(name: "Model")
-            container.loadPersistentStores { (_, error) in
-                if let error = error as NSError? {
-                    fatalError("Unresolved error \(error), \(error.userInfo)")
-                }
+    static let persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Model")
+        container.loadPersistentStores { (_, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
             }
-            return container
-        }()
+        }
+        return container
+    }()
 
     static var context: NSManagedObjectContext { return persistentContainer.viewContext }
 
     static func saveContext () {
         let context = persistentContainer.viewContext
-
-            guard context.hasChanges else {
-                return
+        guard context.hasChanges else {
+            return
         }
-            do {
-                try context.save()
-            } catch {
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
+        do {
+            try context.save()
+        } catch {
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
         }
+    }
 
 }
