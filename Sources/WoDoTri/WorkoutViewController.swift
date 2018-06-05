822312e2f0f1cdbf0fbc2c819cba56db2440e655
Markus Staas <markus.staas@me.com>
2018-06-05 17:56:49 +0100

    Add locationManager.distanceFilter to update every 20 meters

--- Sources/WoDoTri/WorkoutViewController.swift
+++ Sources/WoDoTri/WorkoutViewController.swift
@@ -30,6 +30,8 @@ final class WorkoutViewController: UIViewController {
         locationManager.delegate = self
         locationManager.requestAlwaysAuthorization()
         locationManager.startUpdatingLocation()
+        locationManager.distanceFilter = 20
+        
         persistentContainer.loadPersistentStores { _, _ in }
         let workoutType = dataSource.workoutType(for: self)
         workout = Workout(workoutType: workoutType, managedObjectContext: persistentContainer.viewContext)
