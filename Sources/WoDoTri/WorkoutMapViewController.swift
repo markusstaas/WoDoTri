76a3b584bd87b23911500a2308f84eb18eeb0980
Markus Staas <markus.staas@me.com>
2018-06-13 10:07:14 +0300

    Rename centerOnMap to centerOnUserLocationButton

--- Sources/WoDoTri/WorkoutMapViewController.swift
+++ Sources/WoDoTri/WorkoutMapViewController.swift
@@ -4,15 +4,48 @@ import MapKit
 final class WorkoutMapViewController: UIViewController {
 
     @IBOutlet weak private var mapView: MKMapView!
+<<<<<<< ours
 
     @IBAction func centerOnMapTapped(_ sender: Any) {
         mapView.userTrackingMode = .followWithHeading
     }
     override func viewDidLoad() {
+=======
+    @IBOutlet weak private var centerMapOnUserLocationButton: UIButton!
+
+    @IBAction func centerOnMapTapped() {
+        mapView.userTrackingMode = .followWithHeading
+        centerMapOnUserLocationButton.isHidden = true
+    }
+    override func viewDidLoad() {
+
+        let mapDragRecognizer = UIPanGestureRecognizer(target: self, action: #selector(self.didDragMap(gestureRecognizer:)))
+        mapDragRecognizer.delegate = self
+
+        centerMapOnUserLocationButton.isHidden = true
+>>>>>>> theirs
         mapView.userTrackingMode = .followWithHeading
         mapView.showsScale = true
         mapView.showsCompass = true
         mapView.isZoomEnabled = false
 
     }
+<<<<<<< ours
+=======
+    @objc private func didDragMap(gestureRecognizer: UIGestureRecognizer) {
+        if gestureRecognizer.state == UIGestureRecognizerState.began {
+            centerMapOnUserLocationButton.isHidden = false
+        }
+    }
+}
+
+// MARK: - Manage UIGestureRecognizerDelegate
+
+extension WorkoutMapViewController: UIGestureRecognizerDelegate {
+
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+
+>>>>>>> theirs
 }
