019916ffacbcab953e0a343f62c6a7a841f6c560
Markus Staas <markus.staas@me.com>
2018-09-04 12:37:26 +0100

    Add Workout Finished VC and segue

--- Sources/WoDoTri/WorkoutPausedViewController.swift
+++ Sources/WoDoTri/WorkoutPausedViewController.swift
@@ -10,16 +10,31 @@ class WorkoutPausedViewController: UIViewController {
 
     weak var delegate: StartWorkoutButtonDelegate?
 
+    private var workoutFinishedViewController: WorkoutFinishedViewController?
+    private let workoutFinishedViewControllerSegueIdentifier = "Workout Finished View Controller Segue"
+
     @IBAction func startWorkout() {
         self.delegate?.startOrPauseWorkout()
         self.dismiss(animated: true, completion: nil)
     }
 
     @IBAction func finishWorkout() {
+        performSegue(withIdentifier: workoutFinishedViewControllerSegueIdentifier, sender: self)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+
+    // MARK: - Handling Storyboard Segues
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        if let destinationWorkoutFinishedViewController = segue.destination as? WorkoutFinishedViewController {
+            workoutFinishedViewController = destinationWorkoutFinishedViewController
+            //destinationWorkoutPausedViewController.delegate = self
+        }
+    }
+
 }
