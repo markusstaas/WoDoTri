fb1603295ae530b15cdbfd99f21921c47de74d55
Markus Staas <markus.staas@me.com>
2018-09-04 12:37:52 +0100

    Add WorkoutFinishedViewController class

---
+++ Sources/WoDoTri/WorkoutFinishedViewController.swift
@@ -0,0 +1,12 @@
+//  Copyright © 2018 Markus Staas. All rights reserved.
+
+import UIKit
+
+class WorkoutFinishedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
